271ec722db9c20fdbd5e14550a3b50e0777c4dfa
jpatino <jpatino1806@gmail.com>
2024-08-10 21:09:00 -0500

    agrega Contador.vue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import Contador from "./Contador.vue";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+const app = createApp(Contador);
 
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
